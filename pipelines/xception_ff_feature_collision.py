c546a28ffedc4e0a0af3fb91a9c987ec276b6c86
Alexander Murphy <lordpizza11@gmail.com>
2023-12-22 23:11:46 +0100

    .

--- pipelines/xception_ff_feature_collision.py
+++ pipelines/xception_ff_feature_collision.py
@@ -1,11 +1,27 @@
 from attacks import feature_collision, attack_util
 from data import data_util
-from networks import xception
+from networks.xception import get_xception
 
 def __main__(self):
-    network = xception
+    network = get_xception()
     attack = feature_collision
     target = data_util.get_one_fake_ff()
     base = data_util.get_one_real_ff()
 
-    attack_util.run_attack(network, target, base, attack)
+    poisons = attack_util.run_attack(network, target, base, attack)
+
+    eval_network(network)
+
+    retrain_with_poisons(network, poisons)
+
+    eval_poisons(network, poisons)
+    eval_network(network)
+
+def retrain_with_poisons(network, poisons):
+    pass
+
+def eval_network(network):
+    pass
+
+def eval_poisons(network, poisons):
+    pass
---
+++ scripts/videos_to_images_ff.sh
@@ -0,0 +1,2 @@
+cd ../data/ff
+python generate_original_images_from_downloaded_videos.py DATA_PATH -d all -c c23
