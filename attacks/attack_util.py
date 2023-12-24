3dd005e09bcd8acea41b302aa3ac4483ba0d46f6
Alexander Murphy <lordpizza11@gmail.com>
2023-12-24 13:50:45 +0100

    .

--- attacks/attack_util.py
+++ attacks/attack_util.py
@@ -1,5 +1,8 @@
-def run_attack(network, target, base, attack):
+import feature_collision
+
+def run_attack(network, target, base, attack, iterations):
     assert network and target and base and attack, \
     "Didn't receive all parameters to perform attack"
 
-    
+    if attack == "FC":
+        feature_collision.feature_collision(network, target, base, iterations)
