28fc9994a75ce8b3645cd967f3575cd2599c422a
Alexander Murphy <lordpizza11@gmail.com>
2023-12-25 13:03:28 +0100

    .

--- scripts/videos_to_images_ff.sh
+++ scripts/videos_to_images_ff.sh
@@ -1,2 +1,4 @@
 cd ../data/ff
-python generate_original_images_from_downloaded_videos.py DATA_PATH -d all -c c23
+python generate_original_images_from_downloaded_videos.py original_sequences/actors -d all -c c23
+
+#/exports/eddie/scratch/s2017377/Poison_Attacks/data/ff/original_sequences/actors
