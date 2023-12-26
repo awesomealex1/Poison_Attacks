f602c8320f7abef3c0c1f25ac33fd9887a1658fb
Alexander Murphy <lordpizza11@gmail.com>
2023-12-26 16:55:38 +0100

    change job time

--- videos_to_images_all_job.sh
+++ videos_to_images_all_job.sh
@@ -2,7 +2,7 @@
 # Grid Engine options (lines prefixed with #$)
 #$ -N videos_to_images_all            
 #$ -cwd                  
-#$ -l h_rt=00:16:00 
+#$ -l h_rt=16:00:00 
 # -l h_vmem=40G
 # -q gpu 
 # -pe gpu-a100 1
