457a67ac607d8ff4f9845985ae530fb18a68b6d7
Alexander Murphy <lordpizza11@gmail.com>
2023-12-26 16:49:48 +0100

    add parallel scripts

---
+++ delete_all_images.sh
@@ -0,0 +1,7 @@
+rm -rf data/ff/original_sequences/youtube/c23/images
+rm -rf data/ff/original_sequences/actors/c23/images
+rm -rf data/ff/manipulated_sequences/Deepfakes/c23/images
+rm -rf data/ff/manipulated_sequences/Face2Face/c23/images
+rm -rf data/ff/manipulated_sequences/FaceSwap/c23/images
+rm -rf data/ff/manipulated_sequences/NeuralTextures/c23/images
+rm -rf data/ff/manipulated_sequences/FaceShifter/c23/images
