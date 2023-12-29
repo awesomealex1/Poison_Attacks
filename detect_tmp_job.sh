d5d8996d3fc70624cdf55d310b26205b453a5caf
Alexander Murphy <lordpizza11@gmail.com>
2023-12-29 14:24:21 +0100

    .

---
+++ detect_tmp_job.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N videos_to_images_face2face          
+#$ -cwd                  
+#$ -l h_rt=16:00:00 
+#$ -l h_vmem=40G
+#$ -q gpu 
+#$ -pe gpu-a100 1
+#  These options are:
+#  job name: -N
+#  use the current working directory: -cwd
+#  runtime limit of 5 minutes: -l h_rt
+#  memory limit of 1 Gbyte: -l h_vmem
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+cd /exports/eddie/scratch/s2017377/Poison_Attacks
+
+# Load Python
+module load anaconda
+conda activate ff
+
+# Run the program
+. /exports/eddie/scratch/s2017377/Poison_Attacks/detect_tmp.sh
