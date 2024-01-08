1355e2924062c7d4530f387b8a7424c0d0abe005
Alexander Murphy <lordpizza11@gmail.com>
2024-01-08 16:34:28 +0100

    Add code for base image creation

--- data/datasets.py
+++ data/datasets.py
@@ -2,6 +2,8 @@ import torch
 from cv2 import imread
 import os
 from transform import xception_default_data_transforms
+import shutil
+import tqdm
 
 class BaseDataset(torch.utils.data.Dataset):
     def __init__(self):
@@ -25,3 +27,19 @@ class PoisonDataset(torch.utils.data.Dataset):
         img_name = os.path.join(self.root_dir, f'poison_{idx}.png')
         preprocess = xception_default_data_transforms['train']
         return preprocess(imread(img_name)), torch.tensor([1,0])    # Real
+
+def fill_bases_directory(image_paths):
+    if not image_paths:
+        image_paths = []
+        with open('base_images.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                image_paths.append(line.strip())
+        
+    print('Filling bases directory')
+    pb = tqdm.tqdm(total=len(image_paths))
+    for i, image_path in enumerate(image_paths):
+        shutil.copyfile(image_path, f'bases/base_{i}.png')
+        pb.update(1)
+    pb.close()
+    print('Done filling bases directory')
