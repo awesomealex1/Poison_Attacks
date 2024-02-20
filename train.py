d7fb3151b38b8760a44c19a8d8ef852720eace96
Alexander Murphy <lordpizza11@gmail.com>
2024-02-20 23:44:47 +0000

    .

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ def train_on_ff(network, device, dataset=TrainDataset(), name='xception_full_c23
     optimizer = torch.optim.Adam(network.parameters(), lr=lr)
     weight = torch.tensor([4.0, 1.0]).to(device)
     criterion = torch.nn.CrossEntropyLoss(weight=weight)
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=48, pin_memory=True)
     best_score = None
     best_network = None
     for epoch in range(epochs):
@@ -88,7 +88,7 @@ def train_on_ff(network, device, dataset=TrainDataset(), name='xception_full_c23
         save_network(network, f'{name}{epoch}')
         save_training_epoch(name, epoch, total_loss, fake_correct, fake_incorrect, real_correct, real_incorrect)
 
-        eval_network(network, device, name=f'{name}', target=target, fraction_to_eval=1, epoch=epoch)
+        eval_network(network, device, name=f'{name}', target=target, fraction_to_eval=0.01, epoch=epoch)
         score = (fake_correct + real_correct)/(fake_correct + fake_incorrect + real_correct + real_incorrect)
         if best_score is None or score > best_score:
             best_score = score
@@ -137,7 +137,7 @@ def eval_network(network, device, batch_size=100, name='xception_full_c23_traine
     '''Evaluates the network performance on test set.'''
     print('Evaluating network')
     val_dataset = ValDataset()
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=48, pin_memory=True)
     criterion = torch.nn.CrossEntropyLoss()
 
     fake_correct = 0
