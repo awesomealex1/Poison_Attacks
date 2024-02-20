a3e991fe17348d25fd29e2543535fec0b84c22e2
Alexander Murphy <lordpizza11@gmail.com>
2024-02-20 15:23:04 +0000

    .

--- attack_xception_full_baseline.py
+++ attack_xception_full_baseline.py
@@ -161,14 +161,10 @@ def single_poison(feature_space, target, base, max_iters, beta, lr, network, dev
 		print("$$$$$$$$$$$$$$$$$$$$$\n$$$$$$$$$$$$$$$$")
 		print(torch.cuda.memory_summary(device=None, abbreviated=False))
 		target_space = feature_space(target2)
-		print(target_space.element_size()*target_space.nelement())
-		print(target2.element_size()*target_space.nelement())
 		print(torch.cuda.memory_summary(device=None, abbreviated=False))
 		with torch.autograd.profiler.profile(with_stack=True, profile_memory=True) as prof:
 			x_space = feature_space(x2)
-		print(prof.key_averages(group_by_stack_n=5).table(sort_by='self_cpu_time_total', row_limit=5))
-		print(x_space.element_size()*target_space.nelement())
-		print(x2.element_size()*target_space.nelement())
+		print(prof.key_averages(group_by_stack_n=5).table(sort_by='self_cpu_time_total', row_limit=10))
 		print(torch.cuda.memory_summary(device=None, abbreviated=False))
 		feature_space.zero_grad()
 		if i % 10 == 0:
