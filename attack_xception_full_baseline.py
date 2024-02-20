9152878eac470b23c8dcf1d98c93c655085e34d7
Alexander Murphy <lordpizza11@gmail.com>
2024-02-20 14:07:16 +0000

    .

--- attack_xception_full_baseline.py
+++ attack_xception_full_baseline.py
@@ -49,7 +49,10 @@ def main(device, max_iters, beta_0, lr, min_base_score, n_bases, model_path):
 	save_image(target, f'data/targets/{network_name}/target.png')
 	print(torch.cuda.memory_summary(device=None, abbreviated=False))
 	print(f'Original target prediction: {predict_image(network, target, device, processed=False)}')
-	poisons = feature_coll(feature_space, target, max_iters, beta, lr, network, device, network_name=network_name, n_bases=n_bases)
+	try:
+		poisons = feature_coll(feature_space, target, max_iters, beta, lr, network, device, network_name=network_name, n_bases=n_bases)
+	except:
+		print(torch.cuda.memory_summary(device=None, abbreviated=False))
 	save_poisons(poisons, network_name)
 
 	poison_dataset = PoisonDataset(network_name=network_name)
