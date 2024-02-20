b4eced5e8133aed4cae862f6c3525b3d15de3320
Alexander Murphy <lordpizza11@gmail.com>
2024-02-20 20:03:40 +0000

    .

--- train.py
+++ train.py
@@ -48,7 +48,7 @@ def train_on_ff(network, device, dataset=TrainDataset(), name='xception_full_c23
     optimizer = torch.optim.Adam(network.parameters(), lr=lr)
     weight = torch.tensor([4.0, 1.0]).to(device)
     criterion = torch.nn.CrossEntropyLoss(weight=weight)
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
     best_score = None
     best_network = None
     result = subprocess.run(['free', '-m'], capture_output=True, text=True, check=True)
