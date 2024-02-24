a4094488af3c13fbddbbd2e629287f73b5677259
Alexander Murphy <lordpizza11@gmail.com>
2024-02-24 16:16:07 +0000

    .

---
+++ scripts/experiments/baseline/attack_xception_full_50_job.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N attack_xception_full_baseline_50
+#$ -cwd                  
+#$ -l h_rt=48:00:00 
+#$ -l h_vmem=300G
+#$ -q gpu 
+#$ -pe gpu-a100 1
+. /etc/profile.d/modules.sh
+
+cd /exports/eddie/scratch/s2017377/Poison_Attacks
+
+# Load Python
+module load anaconda
+conda activate ff-gpu
+
+# Run the program
+. /exports/eddie/scratch/s2017377/Poison_Attacks/scripts/experiments/baseline/attack_xception_full.sh
