f289cb1bd1d15478211845225cb2c26cfdf806f9
Alexander Murphy <lordpizza11@gmail.com>
2024-03-12 22:15:19 +0000

    .

--- attack_meso_face_baseline_v2.py
+++ attack_meso_face_baseline_v2.py
@@ -135,9 +135,9 @@ def forward_backward(feature_space, target, base, x, beta, lr):
 def forward(feature_space, target, x, lr):
 	detached_x = x.detach()  # Detach x from the computation graph
 	x = detached_x.clone().requires_grad_(True)  # Clone and set requires_grad
-	print(target.shape, x.shape)
-	print(transform(target).shape, transform(x).shape)
-	print(feature_space)
+	#print(target.shape, x.shape)
+	#print(transform(target).shape, transform(x).shape)
+	#print(feature_space)
 	target_space, x_space = feature_space(transform(target)), feature_space(transform(x))
 	distance = torch.norm(x_space - target_space)   # Frobenius norm
 	feature_space.zero_grad()
